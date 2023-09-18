c83efee52d521e10f3a4ac47db8538da8fa57bd2
SU-Lee53 <dltmddnr05@naver.com>
2023-09-18 15:48:47 +0900

    Drill02 제출 (2020184024 이승욱)

--- Drill02/character_grass.py
+++ Drill02/character_grass.py
@@ -1,6 +1,6 @@
 from pico2d import *
-
-open_canvas()
+import math
+open_canvas(800, 600)
 
 # fill here
 
@@ -8,11 +8,44 @@ grass = load_image('grass.png')
 character = load_image('character.png')
 
 x = 0
-while x < 800:
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(x, 90)
-    x = x + 2
-    delay(0.01)
+y = 90
+temp = 0
+midX = 250
+midY = 250
+pi = math.pi
+
+while True:
+    if temp % 2 == 1:
+        for deg in range(270, 630):
+            deg = math.radians(deg)
+            clear_canvas_now()
+            grass.draw_now(400, 30)
+            currX = (midX * math.cos(deg)) + 400
+            currY = (midY * math.sin(deg)) + 300
+            character.draw_now(currX, currY)
+        temp += 1
+    else:
+        for x in range(400, 750):
+            clear_canvas_now()
+            grass.draw_now(400, 30)
+            character.draw_now(x, y)
+
+        for y in range(90, 550):
+            clear_canvas_now()
+            grass.draw_now(400, 30)
+            character.draw_now(x, y)
+    
+        for x in range(750, 50, -1):
+            clear_canvas_now()
+            grass.draw_now(400, 30)
+            character.draw_now(x, y)
+
+        for y in range(550, 90, -1):
+            clear_canvas_now()
+            grass.draw_now(400, 30)
+            character.draw_now(x, y)
+        temp += 1
+                       
+
 
 close_canvas()
