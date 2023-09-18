ad4a43ea8710c65d11ec4357cd4ffcad55414b29
SU-Lee53 <dltmddnr05@naver.com>
2023-09-18 15:50:18 +0900

    Revert "Drill02 제출 (2020184024 이승욱)"
    This reverts commit c83efee52d521e10f3a4ac47db8538da8fa57bd2.

--- Drill02/character_grass.py
+++ Drill02/character_grass.py
@@ -1,6 +1,6 @@
 from pico2d import *
-import math
-open_canvas(800, 600)
+
+open_canvas()
 
 # fill here
 
@@ -8,44 +8,11 @@ grass = load_image('grass.png')
 character = load_image('character.png')
 
 x = 0
-y = 90
-temp = 0
-midX = 250
-midY = 250
-pi = math.pi
-
-while True:
-    if temp % 2 == 1:
-        for deg in range(270, 630):
-            deg = math.radians(deg)
-            clear_canvas_now()
-            grass.draw_now(400, 30)
-            currX = (midX * math.cos(deg)) + 400
-            currY = (midY * math.sin(deg)) + 300
-            character.draw_now(currX, currY)
-        temp += 1
-    else:
-        for x in range(400, 750):
-            clear_canvas_now()
-            grass.draw_now(400, 30)
-            character.draw_now(x, y)
-
-        for y in range(90, 550):
-            clear_canvas_now()
-            grass.draw_now(400, 30)
-            character.draw_now(x, y)
-    
-        for x in range(750, 50, -1):
-            clear_canvas_now()
-            grass.draw_now(400, 30)
-            character.draw_now(x, y)
-
-        for y in range(550, 90, -1):
-            clear_canvas_now()
-            grass.draw_now(400, 30)
-            character.draw_now(x, y)
-        temp += 1
-                       
-
+while x < 800:
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(x, 90)
+    x = x + 2
+    delay(0.01)
 
 close_canvas()
