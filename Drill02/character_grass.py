ddccbe02cc8c16c2588396e0e6bcf2505d722611
SU-Lee53 <dltmddnr05@naver.com>
2023-09-18 17:36:35 +0900

    Drill02 제출 (2020184024 이승욱)

--- Drill02/character_grass.py
+++ Drill02/character_grass.py
@@ -1,5 +1,8 @@
 from pico2d import *
 import math
+import os
+
+os.chdir('C:\\Users\\dltmd\\OneDrive\\Documents\\2-2\\2DGP\\Drill02')
 open_canvas(800, 600)
 
 # fill here
@@ -7,43 +10,62 @@ open_canvas(800, 600)
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-x = 0
-y = 90
+
 temp = 0
 midX = 250
 midY = 250
 pi = math.pi
 
 while True:
-    if temp % 2 == 1:
+    if temp % 2 == 0:
         for deg in range(270, 630):
             deg = math.radians(deg)
             clear_canvas_now()
             grass.draw_now(400, 30)
             currX = (midX * math.cos(deg)) + 400
-            currY = (midY * math.sin(deg)) + 300
+            currY = (midY * math.sin(deg)) + 340
             character.draw_now(currX, currY)
+            delay(0.01)
         temp += 1
+        
     else:
-        for x in range(400, 750):
+        x = 400
+        y = 90
+        while x <= 750:
             clear_canvas_now()
             grass.draw_now(400, 30)
             character.draw_now(x, y)
+            delay(0.01) 
+            x += 3
 
-        for y in range(90, 550):
+        while y <= 550:
             clear_canvas_now()
             grass.draw_now(400, 30)
             character.draw_now(x, y)
-    
-        for x in range(750, 50, -1):
-            clear_canvas_now()
-            grass.draw_now(400, 30)
-            character.draw_now(x, y)
+            delay(0.01) 
+            y += 3
 
-        for y in range(550, 90, -1):
+        while x >= 50:
             clear_canvas_now()
             grass.draw_now(400, 30)
             character.draw_now(x, y)
+            delay(0.01) 
+            x -= 3
+
+        while y >= 90:
+            clear_canvas_now()
+            grass.draw_now(400, 30)
+            character.draw_now(x, y)
+            delay(0.01) 
+            y -= 3
+
+        while x <= 400:
+            clear_canvas_now()
+            grass.draw_now(400, 30)
+            character.draw_now(x, y)
+            delay(0.01) 
+            x += 3
         temp += 1
-                       
+    
+    
 close_canvas()
